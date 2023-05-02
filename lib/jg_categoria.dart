f60c36741382cbef41e9205fddc38e1b043cda85
alanborgess <alanborges942@gmail.com>
2023-05-02 13:12:49 -0300

    interface_app

---
+++ lib/jg_categoria.dart
@@ -0,0 +1,49 @@
+
+
+  Map<String, List<String>> imagensPorCategoria = {
+  'Ação': [
+    'assets/images/acao1.jpg',
+    'assets/images/acao2.jpg',
+    'assets/images/acao3.jpg',
+  ],
+  'Simulação': [
+    'assets/images/simulacao1.jpg',
+    'assets/images/simulacao2.jpg',
+    'assets/images/simulacao3.jpg',
+  ],
+  'Aventura': [
+    'assets/images/aventura1.jpg',
+    'assets/images/aventura2.jpg',
+    'assets/images/aventura3.jpg',
+  ],
+  'RPG': [
+    'assets/images/rpg1.jpg',
+    'assets/images/rpg2.jpg',
+    'assets/images/rpg3.jpg',
+  ],
+  'Estrategia': [
+    'assets/images/estrateja1.jpg',
+    'assets/images/estrateja2.jpg',
+    'assets/images/estrateja3.jpg',
+  ],
+  'Terror': [
+    'assets/images/terror1.jpg',
+    'assets/images/terror2.jpg',
+    'assets/images/terror3.jpg',
+  ],
+  'Educacional': [
+    'assets/images/educa1.jpg',
+    'assets/images/educa2.jpg',
+    'assets/images/educa3.jpg',
+  ],
+  'Esporte': [
+    'assets/images/esport3.jpg',
+    'assets/images/esport1.jpg',
+    'assets/images/esport1.jpg',
+  ],
+  'Luta': [
+    'assets/images/luta1.jpg',
+    'assets/images/luta2.jpg',
+    'assets/images/luta3.jpg',
+  ],
+};
